1b171703d5868945dd246f971e4ffdec226c9681
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2023-02-07 23:58:37 -0800

    react webpack works

--- sextant/src/components/DisplayLatency.js
+++ sextant/src/components/DisplayLatency.js
@@ -1,39 +1,19 @@
-// import axios for HTTP request
-// import axios from 'axios';
-
-// import WebSocket from 'ws';
-
-// Create a new React Component, which displays packet latency from Pylon. Your component should open a websocket to
-// ws://localhost:55455/, the endpoint advertised by Pylon. Use this library to work with Websockets.
-import React, { useState, useEffect } from 'react';
-// var W3CWebSocket = require('websocket').w3cwebsocket;
-
-// var client = new W3CWebSocket('ws://localhost:55455/', 'echo-protocol');
-
-const WebSocket = require('ws');
-
-const ws = new WebSocket('ws://localhost:55455/');
-
+import React, { useState } from 'react';
+import useWebSocket from 'react-use-websocket';
 
 const DisplayLatency = () => {
-  const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    ws.on('request', function (message) {
-      console.log(message);
-    })
-  }, [])
+  const [serverTime, setServerTime] = useState(0);
+  const { lastMessage } = useWebSocket('ws://localhost:55455', {
+    onMessage: (event) => {
+      setServerTime(event.data);
+    },
+  });
 
   return (
     <div>
-      <p>Time from server: {time}</p>
+      <h2>Server Time: {serverTime}</h2>
     </div>
   );
 };
 
 export default DisplayLatency;
-
-
-
-
-
