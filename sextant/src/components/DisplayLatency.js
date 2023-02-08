eaacdeb7f01a41aba0f16d2a16e0589510eeed7b
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2023-02-07 17:57:08 -0800

    futile efforts to get webpack worked. Perhaps the best solution is to convert webpack 5 back to 4

--- sextant/src/components/DisplayLatency.js
+++ sextant/src/components/DisplayLatency.js
@@ -4,37 +4,29 @@
 // import WebSocket from 'ws';
 
 // Create a new React Component, which displays packet latency from Pylon. Your component should open a websocket to
-// ws://localhost:55455, the endpoint advertised by Pylon. Use this library to work with Websockets.
+// ws://localhost:55455/, the endpoint advertised by Pylon. Use this library to work with Websockets.
 import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
+// var W3CWebSocket = require('websocket').w3cwebsocket;
 
-// const socket = io.connect('ws://localhost:55455');
-const socket = io("ws://localhost:55455", {
-  transports: ["websocket"]
-});
-// const socket = io("ws://localhost:55455");
+// var client = new W3CWebSocket('ws://localhost:55455/', 'echo-protocol');
+
+const WebSocket = require('ws');
+
+const ws = new WebSocket('ws://localhost:55455/');
+
+
 const DisplayLatency = () => {
   const [time, setTime] = useState(0);
-  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    socket.on('connect', () => {
-      setMessage('server connected')
-    });
-
-    socket.on('message', (timestamp) => {
-      setTime(timestamp);
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+    ws.on('request', function (message) {
+      console.log(message);
+    })
+  }, [])
 
   return (
     <div>
       <p>Time from server: {time}</p>
-      <p>Message: {message}</p>
     </div>
   );
 };
