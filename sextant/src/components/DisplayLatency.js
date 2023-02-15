176724ceaa6631dc6a5685576a607e4f936b7242
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2023-02-14 18:45:46 -0800

    doing well so far

--- sextant/src/components/DisplayLatency.js
+++ sextant/src/components/DisplayLatency.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import useWebSocket from 'react-use-websocket';
+import Chart from 'chart.js/auto';
 
 const DisplayLatency = () => {
   const [serverTime, setServerTime] = useState(0);
@@ -9,19 +10,46 @@ const DisplayLatency = () => {
     },
   });
   const [latency, setLatency] = useState(0);
-
-  // Every time your component receives a new message, it should determine the associated packet latency by subtracting the packet timestamp from the current time.
-
-  // Once you’ve figured out the latency, you should display this metric to the user in a new container component, just like you did with IP addresses in the last step.
+  const chartContainer = useRef(null);
 
   useEffect(()=>{
     const currentTime = new Date().getTime();
     setLatency(currentTime - serverTime);
   }, [serverTime])
 
+  useEffect(() => {
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+
+    const chart = new Chart(chartContainer.current, {
+      type: 'line',
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    });
+
+    return () => {
+      chart.destroy();
+    };
+  }, []);
+
   return (
     <div>
       <h2>Latency: {latency}</h2>
+      <canvas className="bg-slate-50" ref={chartContainer} id="acquisitions" />
     </div>
   );
 };
