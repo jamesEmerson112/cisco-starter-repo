49077e25e5c53c4f3ea5671b09ce03c08944570c
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2023-02-18 18:42:58 -0800

    Added duration

--- sextant/src/components/DisplayLatency.js
+++ sextant/src/components/DisplayLatency.js
@@ -6,80 +6,78 @@ import StreamingPlugin from 'chartjs-plugin-streaming';
 
 Chart.register(StreamingPlugin);
 
+// Global variable to hold data
 let data = [{x: Date.now(), y: 0}];
 
 const DisplayLatency = () => {
   const [serverTime, setServerTime] = useState(0);
+  const [delayCounter, setDelayCounter] = useState(0);      // delay the data push process to not overwhelm the visualization
   const { lastMessage } = useWebSocket('ws://localhost:55455', {
     onMessage: (event) => {
       setServerTime(event.data);
+      setDelayCounter(prev => prev + 1);
     },
   });
   const [latency, setLatency] = useState(data);
 
+  // Update the latency
   useEffect(()=>{
-    const currentTime = new Date().getTime();
-    const latencyResult = serverTime !== 0 ? currentTime - serverTime : 0;
-    if (latency.length > 500) {
-      setLatency(prevLatency => prevLatency.splice(400));
+    if (delayCounter % 10 === 0) {
+      const currentTime = new Date().getTime();
+      const latencyResult = serverTime !== 0 ? currentTime - serverTime : 0;
+      if (latency.length > 500) {
+        setLatency(prevLatency => prevLatency.splice(400));
+      }
+      setLatency(prevLatency => [...prevLatency, {x: Date.now(), y: latencyResult}]);
     }
-    setLatency(prevLatency => [...prevLatency, {x: Date.now(), y: latencyResult}]);
   }, [serverTime])
 
-  console.log(latency);
-
-  // // ==================================
-  // // Memo
-  // // const lineChart = useMemo(() => {
-
-  // //   return <LineChart data={latency} />
-  // // }, [latency])
-
-  // const memoizedLatency = useMemo(() => latency, [latency])
-
-  // console.log('check')
-
-  // return <div>
-  //   <h2>{memoizedLatency}</h2>
-  // </div>
-
+  // dynamic Line Chart
   const lineChart =
-  <Line
+  <Line className="bg-slate-100"
   data={{
     datasets: [{
       label: 'Latency overtime',
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgba(54, 162, 235, 0.5)',
-      borderDash: [8, 4],
       fill: true,
-      data: latency
+      data: latency,
+      cubicInterpolationMode: 'monotone',
     }]
   }}
   options={{
+    animation: true,
+    plugins: {
+      streaming: {
+        frameRate: 30   // chart is drawn 5 times every second
+      }
+    },
     scales: {
       x: {
         type: 'realtime',
         realtime: {
-          delay: 3000,
+          duration: 20000,
+          delay: 2000,
         },
         title: {
           display: true,
           text: 'Time'
-        }
+        },
       },
       y: {
         title: {
           display: true,
-          text: 'Latency'
-        }
-      }
-    }
+          text: 'Latency (ms)',
+        },
+        suggestedMin: 0,
+        suggestedMax: 10
+      },
+    },
   }}
 />
 
 
   return <div>
-    <h2>{latency[latency.length-1].y}</h2>
     {lineChart}
   </div>
 
